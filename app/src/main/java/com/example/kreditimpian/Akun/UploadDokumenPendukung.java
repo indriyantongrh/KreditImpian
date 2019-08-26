502593b12e4eb4a8fc7d7c7081b67af0f3a2319f
Universe Developer <universedeveloper8@gmail.com>
2019-08-26 07:32:05 +0700

    update UI

---
+++ app/src/main/java/com/example/kreditimpian/Akun/UploadDokumenPendukung.java
@@ -0,0 +1,16 @@
+package com.example.kreditimpian.Akun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kreditimpian.R;
+
+public class UploadDokumenPendukung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_dokumen_pendukung);
+    }
+}
