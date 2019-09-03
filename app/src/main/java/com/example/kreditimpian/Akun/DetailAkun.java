6ad7c5f3fafe3499cfa0547a3aca0bb012e07d92
masyantoya@gmail.com <asolole123>
2019-09-03 11:26:26 +0700

    Update User Interface - Add Font jipatharegular

--- app/src/main/java/com/example/kreditimpian/Akun/DetailAkun.java
+++ app/src/main/java/com/example/kreditimpian/Akun/DetailAkun.java
@@ -1,16 +1,31 @@
 package com.example.kreditimpian.Akun;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.kreditimpian.R;
 
 public class DetailAkun extends AppCompatActivity {
 
+    CardView btndatapribadi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_akun);
+
+        btndatapribadi = findViewById(R.id.btndatapribadi);
+        btndatapribadi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DataDiri.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/kreditimpian/Akun/FragmentAkun.java
+++ app/src/main/java/com/example/kreditimpian/Akun/FragmentAkun.java
@@ -1,15 +1,18 @@
 package com.example.kreditimpian.Akun;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.kreditimpian.MenuUtama.MenuUtama;
 import com.example.kreditimpian.R;
 
 /**
@@ -25,6 +28,8 @@ public class FragmentAkun extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    CardView btndetailakun;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -67,7 +72,20 @@ public class FragmentAkun extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_akun, container, false);
+        View view= inflater.inflate(R.layout.fragment_fragment_akun, container, false);
+
+        btndetailakun = view.findViewById(R.id.btndetailakun);
+        btndetailakun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), DetailAkun.class);
+                getActivity().startActivity(intent);
+            }
+
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
