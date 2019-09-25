ab139b2de59473f49950dc43132e0521c0e1dcb2
Universe Developer <universedeveloper8@gmail.com>
2019-09-25 11:01:48 +0700

    Kamera sedang di perbaiki

--- app/src/main/java/com/example/kreditimpian/FormPengajuan/CaptureGambar.java
+++ app/src/main/java/com/example/kreditimpian/FormPengajuan/CaptureGambar.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.app.Activity;
@@ -37,8 +38,11 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import pub.devrel.easypermissions.EasyPermissions;
 
@@ -72,6 +76,15 @@ public class CaptureGambar extends AppCompatActivity {
     int PICK_IMAGE_REQUEST = 1;
     int bitmap_size = 60; // range 1 - 100
 
+    private String pictureFilePath;
+
+
+    String mCurrentPhotoPath;
+    private static final String IMAGE_DIRECTORY_NAME = "VLEMONN";
+    File photoFile = null;
+    static final int CAPTURE_IMAGE_REQUEST = 1;
+    Uri photoURI =null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,7 +99,17 @@ public class CaptureGambar extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-            //pakai alert dialog
+
+/*                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    captureImage();
+                }
+                else
+                {
+                   /// captureImage2();
+            }
+                */
+
+            /////pakai alert dialog
                 android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(CaptureGambar.this);
                 builder.setTitle("Pilihan");
                 builder.setMessage("Silahkan memilih kamera atau galeri");
@@ -95,7 +118,10 @@ public class CaptureGambar extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                         if (takePictureIntent.resolveActivity(CaptureGambar.this.getPackageManager()) != null) {
+
                             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE_1);
+
+
 
                         }
                     }
@@ -111,31 +137,12 @@ public class CaptureGambar extends AppCompatActivity {
                 android.app.AlertDialog alert = builder.create();
                 alert.show();
 
-      /*          ///set permition camera
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-                        ActivityCompat.requestPermissions(CaptureGambar.this, new String[]{Manifest.permission.CAMERA},1);
-                    }
 
-                }
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(),
-                        WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(CaptureGambar.this,
-                            new String[]{WRITE_EXTERNAL_STORAGE}, REQUEST_PERM_WRITE_STORAGE);
-                }else {
-                    takephoto();
-                }*/
 
 
 
             }
-
-
-
-
- /*               Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);*/
-
+            
         });
 
 /*        btnselanjutnya = findViewById(R.id.btnselanjutnya);
@@ -166,6 +173,159 @@ public class CaptureGambar extends AppCompatActivity {
     }
 
 
+
+
+
+
+
+
+
+    /* Capture Image function for 4.4.4 and lower. Not tested for Android Version 3 and 2 */
+    private void captureImage2() {
+
+        try {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            photoFile = createImageFile4();
+            if(photoFile!=null)
+            {
+                displayMessage(getBaseContext(),photoFile.getAbsolutePath());
+                Log.i("Mayank",photoFile.getAbsolutePath());
+                Uri photoURI  = Uri.fromFile(photoFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(cameraIntent, CAPTURE_IMAGE_REQUEST);
+            }
+        }
+        catch (Exception e)
+        {
+            displayMessage(getBaseContext(),"Camera is not available."+e.toString());
+        }
+    }
+
+    private void captureImage()
+    {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+        else {
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQUEST);
+
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                // Create the File where the photo should go
+                try {
+
+                    photoFile = createImageFile();
+                    displayMessage(getBaseContext(),photoFile.getAbsolutePath());
+                    Log.i("Mayank",photoFile.getAbsolutePath());
+
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(this,
+                                "com.example.kreditimpian.fileprovider",
+                                photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQUEST);
+                    }
+                } catch (Exception ex) {
+                    // Error occurred while creating the File
+                    displayMessage(getBaseContext(),ex.getMessage().toString());
+                }
+
+
+            }else
+            {
+                displayMessage(getBaseContext(),"Nullll");
+            }
+        }
+
+    }
+
+
+/*    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+     ///   Bundle extras = data.getExtras();
+      ///  Bitmap imageBitmap = (Bitmap) extras.get("data");
+      ////  image.setImageBitmap(imageBitmap);
+
+        if (requestCode == CAPTURE_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            image.setImageBitmap(myBitmap);
+        }
+        else
+        {
+            displayMessage(getBaseContext(),"Request cancelled or something went wrong.");
+        }
+    }*/
+
+    private File createImageFile4()
+    {
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                displayMessage(getBaseContext(),"Unable to create directory.");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void displayMessage(Context context, String message)
+    {
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    }
+
+/*    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                captureImage();
+            }
+        }else {
+            displayMessage(getBaseContext(), "This apps without permition");
+        }
+
+    }*/
+
+
+
+
+
+
+
+    ///////////////////////////////////
+
     //untuk upload image, compress .JPEG ke bitmap
     public String getStringImage(Bitmap bmp) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -194,6 +354,8 @@ public class CaptureGambar extends AppCompatActivity {
         image.setImageBitmap(decoded_1);
 
 
+
+
     }
 
 
@@ -213,6 +375,7 @@ public class CaptureGambar extends AppCompatActivity {
         return Bitmap.createScaledBitmap(image, width, height, true);
     }
 
+
   @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -225,6 +388,8 @@ public class CaptureGambar extends AppCompatActivity {
                 bitmap = MediaStore.Images.Media.getBitmap(CaptureGambar.this.getContentResolver(), filePath);
                 // 512 adalah resolusi tertinggi setelah image di resize, bisa di ganti.
                 setToImageView1(getResizedBitmap(bitmap, 512));
+
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -250,6 +415,13 @@ public class CaptureGambar extends AppCompatActivity {
     }
 
 
+    private void addToGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(pictureFilePath);
+        Uri picUri = Uri.fromFile(f);
+        galleryIntent.setData(picUri);
+        this.sendBroadcast(galleryIntent);
+    }
 
 
     public void sendimage (View v)
@@ -258,6 +430,9 @@ public class CaptureGambar extends AppCompatActivity {
         i.putExtra("resID", bitmap);
         startActivity(i);
     }
+
+
+
 
   /*  public void  takephoto(){
         Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
