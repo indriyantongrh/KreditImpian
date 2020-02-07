5a8fdde7ea4fc6924231af0c3fc323de77ff6280
Indriyantongrh <masyantoya@gmail.com>
2020-02-07 16:32:46 +0700

    Initial Commit Transaction Via Catalog

---
+++ app/src/main/java/com/application/kreditimpian/Model/ModelPengajuanCatalog/ResponsePengajuanCatalog.java
@@ -0,0 +1,41 @@
+package com.application.kreditimpian.Model.ModelPengajuanCatalog;
+
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class ResponsePengajuanCatalog{
+
+	@SerializedName("response_code")
+	private int responseCode;
+
+	@SerializedName("data")
+	private Object data;
+
+	@SerializedName("message")
+	private String message;
+
+	public void setResponseCode(int responseCode){
+		this.responseCode = responseCode;
+	}
+
+	public int getResponseCode(){
+		return responseCode;
+	}
+
+	public void setData(Object data){
+		this.data = data;
+	}
+
+	public Object getData(){
+		return data;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+}
